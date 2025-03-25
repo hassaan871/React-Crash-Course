ca396de38a27d84f15c48c533fd8fb4e9410ebaa
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-03-25 22:26:30 +0500

    writing markup with jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import Login, {secretKey, SignIn} from "./Login"
-// import {Login} from "./Login"
+
 
 function App() {
+  const name = "Hassaan";
+  const x = 10;
+  const y = 20;
   return (
     <div>
-      <h1>Importing and exporting components</h1>
-      <Login/>
-      <SignIn/>
-      <h4>{secretKey}</h4>
+      <h1>Writing markup with JSX</h1>
+
+      <h4>Hey {name}</h4>
+      <h5>{10+20+30}</h5>
+      <h5>{x+y}</h5>
+      <button onClick={()=>{alert("Hello")}}> this is the btn</button>
     </div>
   )
 }
