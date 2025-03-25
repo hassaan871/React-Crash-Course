aee983751dc5433bd96e3ba0a096520a3a258c66
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-03-25 22:39:02 +0500

    JSX exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-
-
 function App() {
-  const name = "Hassaan";
-  const x = 10;
-  const y = 20;
+ function callFun(){
+  alert("function called");
+ }
   return (
     <div>
-      <h1>Writing markup with JSX</h1>
-
-      <h4>Hey {name}</h4>
-      <h5>{10+20+30}</h5>
-      <h5>{x+y}</h5>
-      <button onClick={()=>{alert("Hello")}}> this is the btn</button>
+      <h1>Anil Sidhu Todos</h1>
+      <img 
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMKWxvNYHcce6skb2t0z5zlYmBETHLMl0h6g&s"
+      alt="Anil Sidhu"
+       />
+       <ul>
+        <li>Invent new traffic lights</li>
+        <li>Reherse a movie scene</li>
+        <li>Improve the specturm tech</li>
+       </ul>
+       <button onClick={callFun}>Click Me</button>
     </div>
   )
 }
