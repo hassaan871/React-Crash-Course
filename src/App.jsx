7b2fb0d2bbba553113b930c8f4883e9e743317ef
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-03-25 21:48:36 +0500

    importing and exporting in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,13 @@
+import Login, {secretKey, SignIn} from "./Login"
+// import {Login} from "./Login"
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
-      <Fruit />
-      <Color />
-    </div>
-  )
-}
-
-function Fruit() {
-  return (
-    <div>
-      <h2>Apple</h2>
-    </div>
-  )
-}
-
-function Color() {
-  return (
-    <div>
-      <h2>Red Color</h2>
+      <h1>Importing and exporting components</h1>
+      <Login/>
+      <SignIn/>
+      <h4>{secretKey}</h4>
     </div>
   )
 }
