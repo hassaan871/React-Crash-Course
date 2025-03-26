12c579b80ad2a1b2878903557cdd919d4def1c6b
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-03-26 13:46:28 +0500

    click event and function call

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,33 @@
+function callFun() {
+  alert("OUTER function called");
+}
+
+
 function App() {
- function callFun(){
-  alert("function called");
- }
+  // function callFun() {
+  //   alert("INNER function called");
+  // }
+  function Apple(name) {
+    alert(name);
+  }
+  function Banana(name) {
+    alert(name);
+  }
   return (
     <div>
       <h1>Anil Sidhu Todos</h1>
-      <img 
-      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMKWxvNYHcce6skb2t0z5zlYmBETHLMl0h6g&s"
-      alt="Anil Sidhu"
-       />
-       <ul>
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMKWxvNYHcce6skb2t0z5zlYmBETHLMl0h6g&s"
+        alt="Anil Sidhu"
+      />
+      <ul>
         <li>Invent new traffic lights</li>
         <li>Reherse a movie scene</li>
         <li>Improve the specturm tech</li>
-       </ul>
-       <button onClick={callFun}>Click Me</button>
+      </ul>
+      <button onClick={callFun}>Click Me</button>
+      <button onClick={() => Apple("Apple")}>Apple</button>
+      <button onClick={() => Banana("Banana")}>Banana</button>
     </div>
   )
 }
